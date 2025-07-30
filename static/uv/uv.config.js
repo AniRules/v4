9f53be3f40dabfc6dad0bc9256cb9b6c5e672d3b
AniRules <aporeddy043@rsdmo.org>
2025-07-30 17:41:49 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -5,6 +5,6 @@ self.__uv$config = {
     handler: '/uv/uv.handler.js',
     client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
+    config: 'https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
 };
