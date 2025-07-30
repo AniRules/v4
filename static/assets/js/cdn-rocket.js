7302a469550bd46668d1c3586ef19d06f2af5f9c
AniRules <aporeddy043@rsdmo.org>
2025-07-30 16:58:18 -0500

    Update cdn-rocket.js

--- static/assets/js/cdn-rocket.js
+++ static/assets/js/cdn-rocket.js
@@ -107,7 +107,7 @@ function tabCloak() {
 
 function disableTabCloak() {
   var newTitle = "Doge | V4";
-  var newIcon = "/assets/img/doge.jpg";
+  var newIcon = "https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/assets/img/doge.jpg";
 
   localStorage.setItem("tabTitle", newTitle);
   localStorage.setItem("tabIcon", newIcon);
