fdfd0eff653a24e1382647e4bce69f7033d15a56
AniRules <aporeddy043@rsdmo.org>
2025-07-30 17:57:06 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -2,9 +2,9 @@ self.__uv$config = {
     prefix: '/service/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
+    handler: 'https://cdn.jsdelivr.net/gh/titaniumnetwork-dev/Ultraviolet@main/src/uv.handler.js',
     client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: 'https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    sw: 'https://cdn.jsdelivr.net/gh/titaniumnetwork-dev/Ultraviolet@main/src/uv.sw.js',
 };
