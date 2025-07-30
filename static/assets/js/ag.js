3c4d1bdbff8bc5a21d2b1bc4e7517610edcbb532
AniRules <aporeddy043@rsdmo.org>
2025-07-30 16:48:24 -0500

    Update ag.js

--- static/assets/js/ag.js
+++ static/assets/js/ag.js
@@ -8,7 +8,7 @@ function openAg(url, ag) {
 function retroBowl() {
   document.title = '(1) SalyteLearn';
   var cloakcheckScript = document.createElement("script");
-  cloakcheckScript.src = "/assets/js/anym.js";
+  cloakcheckScript.src = "https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/assets/js/anym.js";
   document.head.appendChild(cloakcheckScript);
 
   var functionsScript = document.createElement("script");
@@ -16,18 +16,18 @@ function retroBowl() {
   document.head.appendChild(functionsScript);
 
   document.body.innerHTML = `
-  <script src="/assets/js/index.js"></script>
-  <script src="/uv/uv.bundle.js"></script>
-  <script src="/uv/uv.bundle.js"></script>
-  <script src="/uv/uv.config.js"></script>
-  <script src="/assets/js/ag.js"></script>
+  <script src="https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/assets/js/index.js"></script>
+  <script src="https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/uv/uv.bundle.js"></script>
+  <script src="https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/uv/uv.bundle.js"></script>
+  <script src="https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/uv/uv.config.js"></script>
+  <script src="https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/assets/js/ag.js"></script>
   <script>
     window.navigator.serviceWorker
       .register("/sw.js", {
         scope: __uv$config.prefix,
       });
   </script>
-  <iframe src="/assets/rb/index.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%; overflow: hidden;"></iframe>
+  <iframe src="https://cdn.jsdelivr.net/gh/AniRules/v4@main/static/assets/rb/index.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%; overflow: hidden;"></iframe>
 `;
 
   function getRandomThreeDigitNumber() {
